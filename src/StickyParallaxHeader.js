35bd4570c2029e5b33b8ed79e8d59a617d428af9
Christoph Hummler <christoph.hummler@gmail.com>
2020-08-08 17:03:33 +0200

    undo change tabsContainerBackgroundColor to green

--- src/StickyParallaxHeader.js
+++ src/StickyParallaxHeader.js
@@ -257,7 +257,7 @@ class StickyParallaxHeader extends Component {
       <View
         style={{
           height: backgroundHeight,
-          backgroundColor: 'green',
+          backgroundColor: tabsContainerBackgroundColor,
           ...(backgroundImage && styles.transparentBackground)
         }}
       >
